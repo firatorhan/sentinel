f2c1126bfa9d815e20e50f92acfbb5370403906f
firatorhan <f-ratorhan@hotmail.com>
2026-05-31 19:28:47 +0300

    Feat: extend unplugin for webpack vite rspack..

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { sentinelPlugin } from "sentinel-vite-plugin";
+import { sentinelVitePlugin } from "sentinel-plugin";
 
 export default defineConfig({
   plugins: [
     react(),
-    sentinelPlugin({
+    sentinelVitePlugin({
       include: ["src/components/**/*.tsx"],
       exclude: ["**/*.stories.tsx", "**/*.test.tsx", "**/ProductButton.tsx"],
     }),
